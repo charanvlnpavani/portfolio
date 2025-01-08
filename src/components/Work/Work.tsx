4601f2571b3c1e6f88d140b7f0618a56461e6e6c
Pvlnc <pvlnc@pvlns-MacBook-Air.local>
2025-01-08 12:27:23 +0530

    I modify the work cards to click in particular content is open

--- src/components/Work/Work.tsx
+++ src/components/Work/Work.tsx
@@ -4,20 +4,19 @@ import { workList } from "@/utils/actionList";
 const Work = () => {
   return (
     <div className="workMain w-full h-auto">
-      <div className="work flex flex-col gap-5 md:gap-20 md:flex md:flex-col md:gap-8  items-center w-[90%] h-[100%] py-10 top-20 md:w-[85%] md:h-[80vh] md:top-32 md:p-10">
-        <h1 className="text-3xl font-bold ">Work</h1>
-        <div className="workCards grid grid-cols-1 grid-rows-1 gap-10 md:grid-cols-2 lg:grid-cols-3 md:gap-24 lg:gap-36 ">
+      <div className="work flex flex-col gap-5 md:gap-20 justify-center items-center w-[90%] h-full py-10 top-20 md:w-[85%] md:h-[80vh] md:top-32 md:p-10">
+        <h1 className="text-3xl font-bold">Work</h1>
+        <div className="workCards grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 md:gap-24 lg:gap-36">
           {workList.map((items, index) => (
-            <div
-              className=" w-[40vh] h-[40vh] md:w-56 md:h-56 lg:w-[40vh] lg:h-[60vh]  bg-white text-white cursor-pointer rounded-3xl p-px  card-wrapper "
-              key={index}
-            >
-              <div className=" exp card-content">
-                <h1 className="text-3xl font-bold justify-center items-center md:text-4xl pt-20 md:pt-64">
-                  {items.name}
-                </h1>
+            <a href={items.link} key={index}>
+              <div className="w-[40vh] h-[40vh] md:w-56 md:h-56 lg:w-[40vh] lg:h-[60vh] bg-white text-white cursor-pointer rounded-3xl p-px card-wrapper">
+                <div className={`${items.image} card-content`}>
+                  <h1 className="text-3xl font-bold flex justify-center items-center md:text-4xl pt-20 md:pt-64">
+                    {items.name}
+                  </h1>
+                </div>
               </div>
-            </div>
+            </a>
           ))}
         </div>
       </div>
